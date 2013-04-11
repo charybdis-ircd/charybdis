a37157ebd64efc519646b0d234d1e7d2201171bc
Mantas Mikulėnas <grawity@gmail.com>
2013-04-11 21:33:05 +0300

    Do not trim nicknames at ~
    First, it's 2013 and I'd be really surprised if anybody still cared
    about ISO-646 support these days.
    
    Second, ISO-646-FI, which the "scandinavian origins" probably refers to,
    did NOT have any letters in place of ~ or ^; it had the same ~ ^ and so
    did all other Scandinavian ISO-646 charsets. The only exception here is
    ISO-646-SE-C, which had Ü ü in those places, but it also had É é in
    place of @ `, so by this logic the code should disallow ` as well.
    
    Fuck that. If this patch hurts ISO-646 users, that's a feature.

--- modules/core/m_nick.c
+++ modules/core/m_nick.c
@@ -123,19 +123,12 @@ mr_nick(struct Client *client_p, struct Client *source_p, int parc, const char *
 		return 0;
 	}
 
-	if(parc < 2 || EmptyString(parv[1]) || (parv[1][0] == '~'))
+	if(parc < 2 || EmptyString(parv[1]))
 	{
 		sendto_one(source_p, form_str(ERR_NONICKNAMEGIVEN),
 			   me.name, EmptyString(source_p->name) ? "*" : source_p->name);
 		return 0;
 	}
-
-	/* due to the scandinavian origins, (~ being uppercase of ^) and ~
-	 * being disallowed as a nick char, we need to chop the first ~
-	 * instead of just erroring.
-	 */
-	if((s = strchr(parv[1], '~')))
-		*s = '\0';
 
 	/* copy the nick and terminate it */
 	rb_strlcpy(nick, parv[1], ConfigFileEntry.nicklen);
@@ -183,18 +176,11 @@ m_nick(struct Client *client_p, struct Client *source_p, int parc, const char *p
 	char nick[NICKLEN];
 	char *s;
 
-	if(parc < 2 || EmptyString(parv[1]) || (parv[1][0] == '~'))
+	if(parc < 2 || EmptyString(parv[1]))
 	{
 		sendto_one(source_p, form_str(ERR_NONICKNAMEGIVEN), me.name, source_p->name);
 		return 0;
 	}
-
-	/* due to the scandinavian origins, (~ being uppercase of ^) and ~
-	 * being disallowed as a nick char, we need to chop the first ~
-	 * instead of just erroring.
-	 */
-	if((s = strchr(parv[1], '~')))
-		*s = '\0';
 
 	/* mark end of grace period, to prevent nickflooding */
 	if(!IsFloodDone(source_p))
