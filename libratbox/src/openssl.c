28817cf6f409a32cf7f6c50b432a85a69692bb29
Patrick Godschalk <argure@argure.nl>
2013-10-22 19:13:51 +0200

    Clean up OpenSSL version check.

--- libratbox/src/openssl.c
+++ libratbox/src/openssl.c
@@ -312,22 +312,18 @@ rb_init_ssl(void)
 		ret = 0;
 	}
 	/* Disable SSLv2, make the client use our settings */
-        SSL_CTX_set_options(ssl_server_ctx, SSL_OP_NO_COMPRESSION | SSL_OP_NO_SSLv2 | SSL_OP_CIPHER_SERVER_PREFERENCE);
-        SSL_CTX_set_verify(ssl_server_ctx, SSL_VERIFY_PEER | SSL_VERIFY_CLIENT_ONCE, verify_accept_all_cb);
+	SSL_CTX_set_options(ssl_server_ctx, SSL_OP_NO_COMPRESSION | SSL_OP_NO_SSLv2 | SSL_OP_CIPHER_SERVER_PREFERENCE);
+	SSL_CTX_set_verify(ssl_server_ctx, SSL_VERIFY_PEER | SSL_VERIFY_CLIENT_ONCE, verify_accept_all_cb);
 
-        /* Cipher preference */
-        #if (OPENSSL_VERSION_NUMBER < 0x10000000)
-                SSL_CTX_set_cipher_list(ssl_server_ctx, "EDH+HIGH:HIGH:!aNULL");
-        #endif
-        #if (OPENSSL_VERSION_NUMBER >= 0x10000000)
-                SSL_CTX_set_cipher_list(ssl_server_ctx, "EECDH+HIGH:EDH+HIGH:HIGH:!aNULL");
-        #endif
-	
-	/* Set ECDHE on OpenSSL 1.00+ */
+	/* Cipher preference */
+	#if (OPENSSL_VERSION_NUMBER < 0x10000000)
+		SSL_CTX_set_cipher_list(ssl_server_ctx, "EDH+HIGH:HIGH:!aNULL");
+	#endif
 	#if (OPENSSL_VERSION_NUMBER >= 0x10000000)
+		SSL_CTX_set_cipher_list(ssl_server_ctx, "EECDH+HIGH:EDH+HIGH:HIGH:!aNULL");
 		SSL_CTX_set_tmp_ecdh(ssl_server_ctx, EC_KEY_new_by_curve_name(NID_secp384r1));
 	#endif
-
+	
 	ssl_client_ctx = SSL_CTX_new(TLSv1_client_method());
 
 	if(ssl_client_ctx == NULL)
