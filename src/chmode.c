1d2ba176ea2aa4863de0ff19e4db1145a9b24b18
William Pitcock <nenolod@dereferenced.org>
2016-04-29 18:59:32 -0500

    ircd: Channel.bants is not a serial but a timestamp.
    Previously, the IRCd would increment bants instead of resyncing the timestamp, causing the potential of
    false negatives from the bancache system.

--- src/chmode.c
+++ src/chmode.c
@@ -295,7 +295,7 @@ add_id(struct Client *source_p, struct Channel *chptr, const char *banid, const 
 
 	/* invalidate the can_send() cache */
 	if(mode_type == CHFL_BAN || mode_type == CHFL_QUIET || mode_type == CHFL_EXCEPTION)
-		chptr->bants++;
+		chptr->bants = rb_current_time();
 
 	return 1;
 }
@@ -325,7 +325,7 @@ del_id(struct Channel *chptr, const char *banid, rb_dlink_list * list, long mode
 
 			/* invalidate the can_send() cache */
 			if(mode_type == CHFL_BAN || mode_type == CHFL_QUIET || mode_type == CHFL_EXCEPTION)
-				chptr->bants++;
+				chptr->bants = rb_current_time();
 
 			return banptr;
 		}
