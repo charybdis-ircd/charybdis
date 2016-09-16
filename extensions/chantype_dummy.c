75980a5e2f6b8bc7ac1130acf3bb8bf5b2318870
William Pitcock <nenolod@dereferenced.org>
2016-09-16 13:55:44 -0500

    extensions: add new module adding ^channels (which are just like #channels)

---
+++ extensions/chantype_dummy.c
@@ -0,0 +1,27 @@
+/* dummy channel type (^): just a global channel type */
+
+#include "stdinc.h"
+#include "modules.h"
+#include "client.h"
+#include "ircd.h"
+
+static const char chantype_desc[] = "Secondary global channel type (^)";
+
+static int _modinit(void);
+static void _moddeinit(void);
+
+DECLARE_MODULE_AV2(chantype_dummy, _modinit, _moddeinit, NULL, NULL, NULL, NULL, NULL, chantype_desc);
+
+static int
+_modinit(void)
+{
+	CharAttrs['^'] |= CHANPFX_C;
+
+	return 0;
+}
+
+static void
+_moddeinit(void)
+{
+	CharAttrs['^'] &= ~CHANPFX_C;
+}
