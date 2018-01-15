2d25ba36e1bbc8a29b73a04d9018ff7b37918bb4
jackal <jackal@jackal.invalid>
2018-01-15 23:17:53 +0200

    autogen.sh styling

--- autogen.sh
+++ autogen.sh
@@ -1,58 +1,59 @@
 #! /bin/sh
 
-TOP_DIR=$(dirname $0)
-LAST_DIR=$PWD
+TOP_DIR="$(dirname $0)"
+LAST_DIR="$PWD"
 
-if test ! -f $TOP_DIR/configure.ac ; then
-   echo "You must execute this script from the top level directory."
-   exit 1
+if test ! -f "${TOP_DIR}/configure.ac"; then
+	echo "You must execute this script from the top level directory."
+	exit 1
 fi
 
-AUTOCONF=${AUTOCONF:-autoconf}
-ACLOCAL=${ACLOCAL:-aclocal}
-AUTOMAKE=${AUTOMAKE:-automake}
-AUTOHEADER=${AUTOHEADER:-autoheader}
-LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
-SHTOOLIZE=${SHTOOLIZE:-shtoolize}
+AUTOCONF="${AUTOCONF:-autoconf}"
+ACLOCAL="${ACLOCAL:-aclocal}"
+AUTOMAKE="${AUTOMAKE:-automake}"
+AUTOHEADER="${AUTOHEADER:-autoheader}"
+LIBTOOLIZE="${LIBTOOLIZE:-libtoolize}"
+SHTOOLIZE="${SHTOOLIZE:-shtoolize}"
 
 run_or_die ()
 {
-   COMMAND=$1
+	COMMAND="$1"
 
-   # check for empty commands
-   if test -z "$COMMAND" ; then
-      echo "No command specified."
-      return 1
-   fi
+	# check for empty commands
+	if test -z "${COMMAND}"; then
+		echo "No command specified."
+		return 1
+	fi
 
-   shift;
+	shift
 
-   # print a message
-   echo "Running $COMMAND $@"
+	# print a message
+	echo "Running ${COMMAND} $@"
 
-   # run or die
-   $COMMAND $@ ;
-   result=$?
-   if test $result -ne 0 ; then
-      echo "$COMMAND failed. (exit code = $result)"
-      exit 1
-   fi
+	# run or die
+	"${COMMAND}" "$@"
+	result="$?"
 
-   return 0
+	if test "${result}" -ne 0; then
+		echo "${COMMAND} failed. (exit code = ${result})"
+		exit 1
+	fi
+
+	return 0
 }
 
-echo "Building librb autotools files."
+echo "Building librb autotools files"
 
-cd "$TOP_DIR"/librb
+cd "${TOP_DIR}"/librb
 sh autogen.sh
 
-echo "Building main autotools files."
+echo "Building main autotools files"
 
-cd "$LAST_DIR"
+cd "${LAST_DIR}"
 
-run_or_die $ACLOCAL -I m4
-run_or_die $LIBTOOLIZE --force --copy
-run_or_die $AUTOHEADER
-run_or_die $AUTOCONF
-run_or_die $AUTOMAKE --add-missing --copy
-run_or_die $SHTOOLIZE all
+run_or_die "${ACLOCAL}" -I m4
+run_or_die "${LIBTOOLIZE}" --force --copy
+run_or_die "${AUTOHEADER}"
+run_or_die "${AUTOCONF}"
+run_or_die "${AUTOMAKE}" --add-missing --copy --foreign
+run_or_die "${SHTOOLIZE}" all
