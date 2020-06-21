0f0de3478c03f3db6034850133311801604b6b4c
Ariadne Conill <ariadne@dereferenced.org>
2020-06-21 05:32:31 +0000

    ircd: fix burst_TS6() of admins

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -697,6 +697,8 @@ burst_TS6(struct Client *client_p)
 			msptr = uptr->data;
 
 			tlen = strlen(use_id(msptr->client_p)) + 1;
+			if(is_admin(msptr))
+				tlen++;
 			if(is_chanop(msptr))
 				tlen++;
 			if(is_voiced(msptr))
